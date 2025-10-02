8b973d0dca5268d8d0e8f5d9bed276db7e4b5b3e
gustavo-jezini <gustavojezini@gmail.com>
2025-10-01 23:07:17 -0300

    feat(app): implement header and navigation components
    - create header component with logo, navigation, and user dropdown
    - implement navigation items component with dynamic active link highlighting
    - add user dropdown component with avatar and menu items
    - update root layout
    - rename app/page.tsx to app/(root)/page.tsx
    - install necessary dependencies: @radix-ui/react-avatar, @radix-ui/react-dropdown-menu, lucide-react, next-themes
    - add constants file to store navigation items
    - create avatar and dropdown-menu components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="flex justify-center items-center h-screen">
+  return (
+  <div className="flex home-wrapper min-h-screen">
     <Button>Click</Button>
-  </div>;
+  </div>
+  );
 }
